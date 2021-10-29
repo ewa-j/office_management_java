13ef215d62e80b914d6a43358729f7f4529bdae7
ewa <ewa.jablonskaa@gmail.com>
2021-10-29 11:03:51 +0100

    FEATURE: adding rooms to the office

--- app/src/main/java/office_management_java/Office.java
+++ app/src/main/java/office_management_java/Office.java
@@ -1,5 +1,20 @@
 package office_management_java;
 
+import java.util.jar.Attributes.Name;
+import java.util.ArrayList;
+
 public class Office {
 
+  ArrayList<Room> rooms;
+  Room room;
+
+  public Office() {
+    rooms = new ArrayList<Room>();
+    room = new Room(null);
+  }
+
+  public Room add(Room room) {
+    rooms.add(room);
+    return room;
+  }
 }
--- app/src/main/java/office_management_java/Room.java
+++ app/src/main/java/office_management_java/Room.java
@@ -5,8 +5,8 @@ public class Room {
   boolean available;
   String roomName;
 
-  public Room(String name) {
-    roomName = name;
+  public Room(String roomName) {
+    this.roomName = roomName;
     available = true;
   }
 
